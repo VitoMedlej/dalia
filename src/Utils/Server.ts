b106860b16932bf46068ac6a057d41b75f3832c6
vito medlej <vito.medlej@gmail.com>
2023-06-27 21:48:58 +0300

    build fix

--- src/Utils/Server.ts
+++ src/Utils/Server.ts
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : `${process.env.NEXT_PUBLIC_URL || 'https://site.coms'}`;
+
+export const server = dev ? 'http://localhost:3000' : `${process.env.NEXT_PUBLIC_URL || 'https://namliieh.netlify.app/'}`;
