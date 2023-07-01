1c32c389ee6840553e0ab6e5e6366d65d0da0b76
vito medlej <vito.medlej@gmail.com>
2023-07-01 17:05:33 +0300

    no cache

--- src/app/[category]/products/page.tsx
+++ src/app/[category]/products/page.tsx
@@ -1,24 +1,19 @@
-import Preloader2 from "@/Components/Preloader2";
-import { server } from "@/Utils/Server";
+import Preloader2 from "@/Components/Preloader2"
+import {server} from "@/Utils/Server"
 
-const Page = ({ data } : any) => {
-  return <Preloader2 data={data} />;
-};
+const Page = async(ctx : any) => {
+    const pageNB = 0;
+    const {category} = ctx.params;
+    console.log('category: ', category);
 
-export async function getEdgeProps({ params } : any) {
-  const pageNB = 0;
-  const { category } = params;
-  const req = await fetch(
-    `${server}/api/fetch-all?page=${pageNB}&category=${
-      category ? `${category}`.replace(/-/g, " ") : "collection"
-    }`
-  );
-  const res = await req.json();
-  return {
-    props: {
-      data: res?.data,
-    },
-  };
+    const req = await fetch(`${server}/api/fetch-all?page=${pageNB}&category=${category
+        ? `${category}`.replace(/-/g, ' ')
+        : 'collection'}`,{cache: 'no-cache'})
+    const res = await req.json()
+    console.log('res: ', res);
+
+    return (<Preloader2 data={res
+        ?.data}/>)
 }
 
-export default Page;
+export default Page
