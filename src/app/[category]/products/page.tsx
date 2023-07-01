c35a950d77b6bfd289726a7b02045c0f15c93112
vito medlej <vito.medlej@gmail.com>
2023-07-01 16:57:14 +0300

    api

--- src/app/[category]/products/page.tsx
+++ src/app/[category]/products/page.tsx
@@ -1,55 +1,24 @@
-// "use client"
+import Preloader2 from "@/Components/Preloader2";
+import { server } from "@/Utils/Server";
 
-import Preloader2 from "@/Components/Preloader2"
-import { server } from "@/Utils/Server"
+const Page = ({ data } : any) => {
+  return <Preloader2 data={data} />;
+};
 
-
-
-
-const Page = async (ctx:any) => {
+export async function getEdgeProps({ params } : any) {
   const pageNB = 0;
-  const {category} = ctx.params;
-  // const [pageNB,setPageNB] = useState(0)
-  // const router = useRouter()
-
-
-
-    // const [data,setData] = useState< {
-    //     products: IProduct[] | never[] ; 
-       
-    // }>({
-    //     products : [],
-       
-    //   })
-  
-    const req = await fetch(`${server}/api/fetch-all?page=${pageNB}&category=${`${category}`.replace(/-/g, ' ') || null}`)
-    const res = await req.json() 
-      //  const InitialFetch = async () => {
-      //   try {
-      
-      //     const req = await fetch(`${server}/api/fetch-all?page=${pageNB}&category=${ category.replace(/-/g, ' ')}`)
-      //     const res = await req.json()
-        
-      //     if (res?.success && res?.data) {
-      //       setData(res?.data)
-      //     }
-      //     return null
-      //   }
-      //   catch(er) {
-      //     console.log('er getAll: ', er);
-      
-      //   }
-      // }
-      // useEffect(() => {
-        
-      //   InitialFetch()
-    
-      // }, [])
-      
-
-    return (
-        <Preloader2 data={res?.data}/>
-    )
+  const { category } = params;
+  const req = await fetch(
+    `${server}/api/fetch-all?page=${pageNB}&category=${
+      category ? `${category}`.replace(/-/g, " ") : "collection"
+    }`
+  );
+  const res = await req.json();
+  return {
+    props: {
+      data: res?.data,
+    },
+  };
 }
 
-export default Page
+export default Page;
