8deb1868bbeb2f9c214441bde48fba6c5b56dc12
Vito Medlej <vito.medlej@gmail.com>
2023-09-14 11:44:12 +0300

    weird err fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
 try {
 
       // const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{ next: { revalidate: 10 } })
-      const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{ cache: 'no-store' })
+      const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{ cache: 'no-store',next:{revalidate:0} })
       let res = await req.json()
       const reqImages = await fetch(`https://getpantry.cloud/apiv1/pantry/732d3c8c-d53a-4c4c-830c-fe9b7e021958/basket/Images`,{  cache:'no-store', next: { revalidate: 400 } })
       let resImages : any = await  reqImages.json();
