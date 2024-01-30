fa1c26626ef36e6bd8803a7d8cca7ff83b714e8d
Vito Medlej <vito.medlej@gmail.com>
2024-01-30 17:55:56 +0200

    bin

--- src/Components/Preloader3.tsx
+++ src/Components/Preloader3.tsx
@@ -13,29 +13,55 @@ import useLanguage from '@/Hooks/useLanguage';
 
 
 const Preloader3 = ({res}:{res:any}) => {
+    const fetchDataAndSetImgs = async () => {
+        try {
+          const response = await fetch('https://api.jsonbin.io/v3/b/65b919d41f5677401f28610e', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-Master-Key': '$2a$10$iAUMdzZeDAI2qTW61Uz6UuE/S8Bgs4LfVuZxwByPpocU4CuR7Nzo2'
+            }
+          });
+      
+          if (!response?.ok) {
+            throw new Error(`Failed to fetch data. Status: ${response?.status}`);
+          }
+      
+          const data = await response.json();
+          setImgs(data?.record); // Assuming 'record' is the property containing the images in the response
+          console.log('Data fetched and set to setImgs:', setImgs);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+      
+    useEffect(() => {
+      
+    
+       fetchDataAndSetImgs()
+    }, [])
+      
+    
     const router = useRouter()
+
     const [imgs,setImgs] = useState([
         {img:'https://ucarecdn.com/c425edb3-eb3d-4c34-af4c-f20890a0d9c9/image00019.jpg',position:'centerd'},
         {img:'https://ucarecdn.com/af01a569-50de-4537-8434-827715426182/image00008.jpeg',position:'topd'},
-        {img:`https://ucarecdn.com/658f97dc-0a75-446c-9dda-ce16bfbc1bba/1st.jpg`,position:'topd'},
-        // {img:'https://www.zdrowiefit.pl/wp-content/uploads/2022/04/Produkty-pszczele-1536x1024.jpg'},
-    //      {
-    //      img:`https://th.bing.com/th/id/R.4904b6c65c536942a919c111e0e0d734?rik=odGE4dFC0fMQ1g&pid=ImgRaw&r=0`
-    //    }
+        {img:'https://ucarecdn.com/658f97dc-0a75-446c-9dda-ce16bfbc1bba/1st.jpg',position:'topd'},
        
      ])
      const redir = () => {
         router.push('/collection/products')
         // console.log('abc')
      }
-    useEffect(() => {
-        console.log('res: ', res);
-    if (res && res?.MainCarousel && res?.MainCarousel?.length > 0) {
-        console.log('res?.MainCarousel: ', res?.MainCarousel);
-        // console.log('res: ', );
-        setImgs(res?.MainCarousel)
-    }
-    }, [])
+    // useEffect(() => {
+    //     console.log('res: ', res);
+    // if (res && res?.MainCarousel && res?.MainCarousel?.length > 0) {
+    //     console.log('res?.MainCarousel: ', res?.MainCarousel);
+    //     // console.log('res: ', );
+    //     setImgs(res?.MainCarousel)
+    // }
+    // }, [])
   const {text} = useLanguage()
     
     return (
