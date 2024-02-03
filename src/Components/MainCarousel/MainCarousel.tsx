d99be28a023fe5116334ea7c535aa571a1deae7d
Vito Medlej <vito.medlej@gmail.com>
2024-02-03 19:33:05 +0200

    ui

--- src/Components/MainCarousel/MainCarousel.tsx
+++ src/Components/MainCarousel/MainCarousel.tsx
@@ -1,41 +1,168 @@
 "use client"
+import { Box, Container, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import {Swiper, SwiperSlide} from "swiper/react";
+// import {Navigation} from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import { useRouter } from 'next/navigation';
+import { Autoplay } from 'swiper';
+import useLanguage from '@/Hooks/useLanguage';
 
-import Preloader3 from '../Preloader3';
-
-const MainCarousel = ({resImages}: any) => {
 
 
+const Preloader3 = () => {
+    // const fetchDataAndSetImgs = async () => {
+    //     try {
+    //       const response = await fetch('https://api.jsonbin.io/v3/b/65b919d41f5677401f28610e', {
+    //         method: 'GET',
+    //         headers: {
+    //           'Content-Type': 'application/json',
+    //           'X-Master-Key': '$2a$10$iAUMdzZeDAI2qTW61Uz6UuE/S8Bgs4LfVuZxwByPpocU4CuR7Nzo2'
+    //         }
+    //       });
+      
+    //       if (!response?.ok) {
+    //         throw new Error(`Failed to fetch data. Status: ${response?.status}`);
+    //       }
+      
+    //       const data = await response.json();
+    //       setImgs(data?.record); // Assuming 'record' is the property containing the images in the response
+    //       console.log('Data fetched and set to setImgs:', setImgs);
+    //     } catch (error) {
+    //       console.error('Error fetching data:', error);
+    //     }
+    //   };
+      
+    // useEffect(() => {
+      
+    
+    //    fetchDataAndSetImgs()
+    // }, [])
+      
+    
+    const router = useRouter()
 
-    try {
-
-        // const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{ next: { revalidate: 10 } })
-        // const req = await fetch(`https://getpantry.cloud/apiv1/pantry/11c7944e-bdec-4962-bcad-f24fe432bca6/basket/Images`,{ next: { revalidate: 400 } })
-        // let res : any = await  req.json() ;
-        // let res = {data:null}
-        return (
-          <Preloader3 res={resImages}/>
-         )
-  }
-  catch (e) {
-    console.log('e home: ', e);
+    const [imgs,setImgs] = useState([
+        {img:'https://th.bing.com/th/id/R.7e46a87cc4b15fdbf0ba6e2c5c34f4bb?rik=OXGkoIf%2bI9kOLQ&riu=http%3a%2f%2fwww.alahlamsy.com%2fpicture%2fClipboard55.jpg&ehk=fOyHQknprh2tIYV%2f8VRFs63V9SiOETAdRejjPzEikTY%3d&risl=&pid=ImgRaw&r=0'},
+        {img:'https://th.bing.com/th/id/R.8b01cb2dc0b885c8d5188e7ba9d2c4f1?rik=%2bYskn20HBuuadA&riu=http%3a%2f%2fwww.alahlamsy.com%2fpicture%2fClipboard44.jpg&ehk=qhCnyIBa2NFEu5wvsjf%2fJCKx12RWeFvesfLzbBNcVn4%3d&risl=&pid=ImgRaw&r=0',position:'centerd'},
+      
+       
+     ])
+     const redir = () => {
+        router.push('/collection/products')
+        // console.log('abc')
+     }
+    // useEffect(() => {
+    //     console.log('res: ', res);
+    // if (res && res?.MainCarousel && res?.MainCarousel?.length > 0) {
+    //     console.log('res?.MainCarousel: ', res?.MainCarousel);
+    //     // console.log('res: ', );
+    //     setImgs(res?.MainCarousel)
+    // }
+    // }, [])
+  const {text} = useLanguage()
+    
     return (
-      <Preloader3 res={resImages || null}/>
-     )
-  
-  }
+        <Box
+            sx={{
+            // py: {xs:'.75em',sm:'2em',md:'3em'},
+            // width: {xs:'98%',md:'74%',lg:'80%'},
+            width:'100%',
+            maxWidth:'none',
+            // maxWidth: 'lg',
+            minHeight:'300px',
+            maxHeight:{sm:'90vh',md:'500px',lg:'550px'},
+            margin: '0 auto',
+            // mt:1,
+            // height : {xs:'500px',sm:'450px',md:'100%'},
+             
+            display: {
+                xs: 'flex'
+            },
+            // mt:20,
+        }}>
+            <Swiper
+            
+                navigation={false}
+                slidesPerView={1}
+                spaceBetween={0}
+                loop={true}
+                autoplay={{
+                delay: 3000,
+                disableOnInteraction: true
+            }}
+                modules={[Autoplay]}
+                className="mySwiper swiper">
+    
+                {imgs.map((item) => {
+    
+                    return <SwiperSlide 
+                     
+                    className='ztop  ' key={item.img}>
+                        <Box
+                            sx={{
+                                position:'relative',
+                                height: '100%',
+                            width:'100%'
+                        }}>
+                        {/* <Container className='auto' sx={{width:'100%'}} maxWidth='lg' disableGutters>
+                            
+                      
+                            <Box sx={{pointerEvents:'none',top:0,right:0,width:'100%',zIndex:1123,height:'100%',background:'black',opacity:.43}} className="absolute">
 
+                            </Box>
+                            <Box 
+                            className='auto'
+                            sx={{
+                                top:'50%',
+                                px:{xs:4,sm:5,md:6},
+                                maxWidth:'650px',
+                                transform:'translateY(-50%)',
+                                zIndex:123456,
+                                position:'absolute'}}>
+                                <Typography 
+                                component='h1'
+                                sx={{color:'white',fontSize:{xs:'2.65em',sm:'3.1em',md:'3.15em',lg:'3em'},
+                                textTransform:'capitalize',
+                                fontWeight:900}}>
+                                {text('THE TASTE OF PURE AUTHENTIC HONEY WITH BEE ORGNAIC', 'استشعر قوة الطبيعة')}
+                                </Typography>
+                                <Typography 
+                                sx={{color:'white',fontSize:{xs:'.85em',sm:'.87em'},mt:1,maxWidth:'700px'}}>
+                                {text('Join the BeeOrganic family today and get a 10% discount on your first purchase!', 'مرحبًا بك في ترو ناشور بليند، متجرك الشامل لجميع احتياجات صحتك وعافيتك. اكتشف تشكيلتنا الرائعة من المكملات الطبيعية الفاخرة، بما في ذلك الأشواغاندا، وتونجكات علي، وفطر كورديسيبس، وفطر رأس الأسد، والمزيد.')}
 
-   
-//      const fetcher = async () => {
-//     const req = await fetch(`https://getpantry.cloud/apiv1/pantry/11c7944e-bdec-4962-bcad-f24fe432bca6/basket/Images`,{ next: { revalidate: 400 } })
-//     let res : any = await  req.json() ;
-//     console.log('res: ', res);
-//     if (res && res?.MainCarousel) {
-//       setImgs(res?.MainCarousel)
-//     }
-//   }
-//   fetcher()
-   
+                                </Typography>
+                                <Box className="flex">
+
+                                <Btn
+                                onClick={()=>redir()}
+                                
+                                className='bg  borderColor' sx={{mt:3}}>
+                                {text('Shop Now', 'تسوق الآن')}
+
+                                </Btn>
+                          
+                                </Box>
+                            </Box>
+                            </Container> */}
+                            <img
+            
+                                className={`img cover  ${item?.position} 
+                                `}
+                                // ${item?.className}
+                                src={`${item.img}`}
+                                alt="Main Carousel Image"/>
+           
+                        </Box>
+                    </SwiperSlide>
+                })
+    }
+    
+            </Swiper>
+    
+        </Box>
+    )
 }
 
-export default MainCarousel
+export default Preloader3
