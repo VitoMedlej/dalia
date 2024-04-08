c6b60074124c464763be03ebdaece95d3d4ec54d
Vito Medlej <vito.medlej@gmail.com>
2024-04-08 17:49:26 +0300

    build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({children} : {
 
                 <ContextWrapper>
                     <Navbar/>
-                    <Sidebar cates={undefined}/>
+                    <Sidebar />
                     <QuickCart/>
                     <ScrollToTop/>
                     <CrispChat/>
